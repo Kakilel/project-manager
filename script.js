7cee635efb4e0ff4796a821237f468629b32b29f
Kakilel Chebarwett <kakilelbarwett@gmail.com>
2025-05-15 12:19:35 +0300

    START JS

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
- function changeText() { 
-            document.getElementById("message").innerHTML = "Hi IM Kakilel!"; 
-        } 
+// Logo
+const logo = document.querySelector(".header-nav header")
+// Nav-items
+const navItems = document.querySelector(".nav-item")
+const [homeNav, financesNav,expensesNav, chartNav] =navItems
+// Sections
+const sections = document.querySelector(".section")
+const [homeSection, financesSection, expensesSection, chartSection] = sections
+//variables
+let
